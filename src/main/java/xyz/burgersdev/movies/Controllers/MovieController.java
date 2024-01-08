e37a4aac33ab6f8fd3ef8b2b1c963ffdae24305a
PugLord06 <86152224+PugLord06@users.noreply.github.com>
2024-01-08 22:55:49 +0200

    Almost finished
    TODO:
    -finish Review submission errors and Image not apperaing

--- src/main/java/xyz/burgersdev/movies/Controllers/MovieController.java
+++ src/main/java/xyz/burgersdev/movies/Controllers/MovieController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import xyz.burgersdev.movies.Movie;
 import xyz.burgersdev.movies.Services.MovieService;
 
 import java.util.List;
 import java.util.Optional;
 
+@EnableWebMvc
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
--- src/main/java/xyz/burgersdev/movies/Controllers/ReviewController.java
+++ src/main/java/xyz/burgersdev/movies/Controllers/ReviewController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import xyz.burgersdev.movies.Review;
 import xyz.burgersdev.movies.Services.ReviewService;
 
 import java.util.Map;
 
+@EnableWebMvc
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
